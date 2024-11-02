8278b794231361c30ce7bb90b41befe53277e796
olavoancay <olavoancay2802@gmail.com>
2024-11-02 17:07:23 -0300

    Adicionando elementos visuais

--- alertbell-frontend/src/App.js
+++ alertbell-frontend/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const notifications = [
+  { id: '1', time: '12:30', title: 'Campainha Tocada' },
+  { id: '2', time: '12:00', title: 'Campainha Tocada' },
+  { id: '3', time: '17:30', title: 'Campainha Tocada' },
+  { id: '4', time: '12:30', title: 'Campainha Tocada' },
+  { id: '5', time: '12:00', title: 'Campainha Tocada' },
+];
+
+const NotificationIcon = ({ name, color, size }) => (
+  <span className="material-icons" style={{ color, fontSize: size || 24 }}>
+    {name}
+  </span>
+);
+
+// Função de renderização de cada item de notificação
+const NotificationItem = ({ item }) => (
+  <div className="notification-item">
+    <NotificationIcon name="notifications" color="#fff" />
+    <span className="notification-title">{item.title}</span>
+    <span className="notification-time">{item.time}</span>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* Main Notification */}
+      <div className="main-notification">
+        <NotificationIcon name="notifications" color="white" size={60} />
+        <div className='main-notification-info'>
+          <span className="main-notification-time">0:38</span>
+          <span className="main-notification-text">Campainha tocada</span>
+        </div>
+      </div>
+
+      {/* Notification List */}
+      <div className="notification-list">
+        {notifications.map((notification) => (
+          <NotificationItem key={notification.id} item={notification} />
+        ))}
+      </div>
     </div>
   );
 }
